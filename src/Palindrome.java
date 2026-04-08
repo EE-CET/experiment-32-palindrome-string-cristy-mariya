d34e5a71ffb7cd6b0d75b206429ba9d9792ffe71
cristy-mariya <151554556+cristy-mariya@users.noreply.github.com>
2026-04-08 14:06:18 +0530

    Update Palindrome.java

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,8 +1,35 @@
+import java.util.*;
+
 public class Palindrome {
-    
-        // TODO: Read the string
-        // TODO: Convert to lower case (to handle 'Malayalam' correctly)
-        // TODO: Check if the string is the same reversed
-        // TODO: Print "1" if palindrome, "0" if not
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        // Convert to lowercase for case-insensitive check
+        str = str.toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        // Output
+        if (isPalindrome) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        sc.close();
+    }
 }
